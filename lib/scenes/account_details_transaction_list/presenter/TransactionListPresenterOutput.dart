58300219533ca87b12151e8cb58bd42e258bba93
Lyle Resnick <lyle@cellarpoint.com>
2019-03-05 00:02:29 -0500

    wip

--- lib/scenes/account_details_transaction_list/presenter/TransactionListPresenterOutput.dart
+++ lib/scenes/account_details_transaction_list/presenter/TransactionListPresenterOutput.dart
@@ -1,4 +1,4 @@
 //  Copyright © 2019 Lyle Resnick. All rights reserved.
 import 'TransactionListViewReadyPresenterOutput.dart';
 
-abstract class TransactionListPresenterOutput extends TransactionListViewReadyPresenterOutput {}
+abstract class TransactionListPresenterOutput with TransactionListViewReadyPresenterOutput {}
